f2822a02a08682b7939b983727455e3a88ad3f4c
aniketxsaini <aniketxsaini@gmail.com>
2026-09-15 23:30:04 +0530

    implement user registration and login endpoints, update user model, and configure user routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import app from "./app.js";
-import {connectToMongoDB} from "./config/mongodb.config.js"
+import {connectToMongoDB} from "./config/mongodb.config.js";
+import {pool} from "./config/pg.config.js"
 const PORT = process.env.PORT || 3000;
 
 const serverStart = async()=>{
     await connectToMongoDB();
+    await pool.connect();
     app.listen(PORT,()=>{
     console.log("server is now running ");
 });
