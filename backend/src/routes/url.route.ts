932ba5781c0229d467c2a49cceb64c3fb4db75d0
aniketxsaini <aniketxsaini@gmail.com>
2026-09-18 22:46:24 +0530

    feat: implement delete URL functionality with Redis cache management

--- backend/src/routes/url.route.ts
+++ backend/src/routes/url.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {createShortUrlController,urlRedirectController} from "../controllers/url.controller.js";
+import {createShortUrlController,urlRedirectController,deleteUrlController} from "../controllers/url.controller.js";
 import {authMiddleware} from "../middlewares/auth.middleware.js"
 router.post('/short',authMiddleware,createShortUrlController);
 router.get('/get/:shortCode',urlRedirectController);
+router.delete('/delete/:shortCode',deleteUrlController);
 export default router;
 
 
